b16fe6a927727a53c50537b217d3d096cebac319
Uiltemberg <trader.uds@gmail.com>
2026-02-23 14:04:39 -0300

    ajuste no whatsApp do dr

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,6 @@ export const CONFIG = {
   PSICOLOGO_CRP: "06/123456",
   PSICOLOGO_ESPECIALIDADE: "Psicólogo Clínico - Especialista em TCC",
   PSICOLOGO_FOTO_URL: "https://images.unsplash.com/photo-1559839734-2b71f153678f?auto=format&fit=crop&q=80&w=600&h=600",
-  PSICOLOGO_WHATSAPP: "5511999999999", // Formato: CódigoPaís + DDD + Número
+  PSICOLOGO_WHATSAPP: "5581920047133", // Formato: CódigoPaís + DDD + Número
   WEBHOOK_URL: "https://webhook.fiqon.app/webhook/019c8666-7a20-73cb-b524-58125d40bcf5/81958579-02cc-4a7c-ba77-8c1f04dc05d3",
 };
