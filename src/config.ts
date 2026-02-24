85496a5ab6aca21a49100c410457bec47d225f68
Uiltemberg <trader.uds@gmail.com>
2026-02-23 23:11:21 -0300

    atualização do webhooks

--- src/config.ts
+++ src/config.ts
@@ -9,5 +9,5 @@ export const CONFIG = {
   PSICOLOGO_ESPECIALIDADE: "Psicólogo Clínico - Especialista em TCC",
   PSICOLOGO_FOTO_URL: "https://images.unsplash.com/photo-1559839734-2b71f153678f?auto=format&fit=crop&q=80&w=600&h=600",
   PSICOLOGO_WHATSAPP: "5581920047133", // Formato: CódigoPaís + DDD + Número
-  WEBHOOK_URL: "https://webhook.fiqon.app/webhook/019c8666-7a20-73cb-b524-58125d40bcf5/81958579-02cc-4a7c-ba77-8c1f04dc05d3",
+  WEBHOOK_URL: "https://webhook.fiqon.app/webhook/019c8d68-a267-729c-b4ba-8dd00432e2c6/0debe017-082e-4601-b468-934d01de2b83",
 };
