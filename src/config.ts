17fb00efcd79507b93ddb4c62484cacc1a0e3c02
Uiltemberg <trader.uds@gmail.com>
2026-02-26 10:50:52 -0300

    ajuste

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@
  */
 
 export const CONFIG = {
-  PSICOLOGO_NOME: "Dr. Dinane Duarte",
+  PSICOLOGO_NOME: "Dr. Dinane Dua",
   PSICOLOGO_CRP: "06/123456",
   PSICOLOGO_ESPECIALIDADE: "Psicólogo Clínico - Especialista em TCC",
   PSICOLOGO_FOTO_URL: "https://images.unsplash.com/photo-1559839734-2b71f153678f?auto=format&fit=crop&q=80&w=600&h=600",
